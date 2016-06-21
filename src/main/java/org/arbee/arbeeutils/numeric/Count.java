aebb322761b990efcc3b4cc8e9432eaddb923383
Richard Ballard <theballards@wave.co.nz>
2016-06-21 13:29:31 +1000

    added Count

---
+++ src/main/java/org/arbee/arbeeutils/numeric/Count.java
@@ -0,0 +1,307 @@
+/*
+ * (C) Copyright 2016 Richard Ballard.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arbee.arbeeutils.numeric;
+
+import net.jcip.annotations.Immutable;
+import org.jetbrains.annotations.NotNull;
+
+import java.math.BigInteger;
+
+/**
+ * This class represents a zero based count.  For any method that creates an instance (e.g. {@link #valueOf(BigInteger, OnExceedBoundary)}
+ * there is an {@code onExceedBoundary} parameter which indicates what should happen if the result would be negative
+ * (which doesn't make sense for a count).
+ */
+@Immutable
+public class Count extends BigIntegerNumber {
+
+    private static final long serialVersionUID = 8108782711353619422L;
+
+    public static final Count ZERO = new Count(BigInteger.ZERO);
+    public static final Count MIN_VALUE = ZERO;
+
+    public enum OnExceedBoundary {
+        /**
+         * e.g. if the value is > MIN_VALUE then set to MIN_VALUE
+         */
+        USE_BOUNDARY_VALUE,
+
+        /**
+         * throw an exception
+         */
+        THROW
+    }
+
+    @NotNull
+    public static Count valueOf(@NotNull final BigInteger value,
+                                @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert value != null;
+        assert onExceedBoundary != null;
+
+        final Count result;
+
+        final int comparisonToZero = value.compareTo(BigInteger.ZERO);
+        if(comparisonToZero == 0) {
+            result = ZERO;
+        }
+        else if(comparisonToZero > 0) {
+            result = new Count(value);
+        }
+        else {
+            if(onExceedBoundary == OnExceedBoundary.THROW) {
+                throw new IllegalArgumentException("value must be >= 0, not " + value);
+            }
+            else if(onExceedBoundary == OnExceedBoundary.USE_BOUNDARY_VALUE) {
+                result = ZERO;
+            }
+            else {
+                throw new IllegalStateException("unknown onExceedBoundary - " + onExceedBoundary);
+            }
+        }
+
+        return result;
+    }
+
+    @NotNull
+    private static BigInteger asBigInteger(@NotNull final BigIntegerNumber value) {
+        assert value != null;
+
+        return value.bigIntegerValue();
+    }
+
+    @NotNull
+    private static BigInteger asBigInteger(final long value) {
+
+        return BigInteger.valueOf(value);
+    }
+
+    @NotNull
+    private static BigInteger asBigInteger(@NotNull final Number value) {
+        assert value != null;
+
+        return asBigInteger(value.longValue());
+    }
+
+
+    @NotNull
+    public static Count valueOf(@NotNull final BigIntegerNumber value,
+                                @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert value != null;
+        assert onExceedBoundary != null;
+
+        return valueOf(asBigInteger(value),
+                       onExceedBoundary);
+    }
+
+    @NotNull
+    public static Count valueOf(final long value,
+                                @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert onExceedBoundary != null;
+
+        return valueOf(asBigInteger(value),
+                       onExceedBoundary);
+    }
+
+    /**
+     *
+     * @param value this will be truncated to an integral value (not rounded)
+     */
+    @NotNull
+    public static Count valueOf(@NotNull final Number value,
+                                @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert value != null;
+        assert onExceedBoundary != null;
+
+        return valueOf(asBigInteger(value),
+                       onExceedBoundary);
+    }
+
+    @NotNull
+    private final BigInteger value;
+
+    private Count(@NotNull final BigInteger value) {
+        assert value != null;
+        assert value.compareTo(BigInteger.ZERO) >= 0;
+
+        this.value = value;
+    }
+
+    /**
+     *
+     * @return the value which will be >= 0
+     */
+    @NotNull
+    public BigInteger getValue() {
+        return value;
+    }
+
+    @NotNull
+    @Override
+    public BigInteger bigIntegerValue() {
+        return value;
+    }
+
+    /**
+     *
+     * @return the value which will be >= 0 (cast to int)
+     */
+    @Override
+    public int intValue() {
+        return value.intValue();
+    }
+
+    /**
+     *
+     * @return the value which will be >= 0
+     */
+    @Override
+    public long longValue() {
+        return value.longValue();
+    }
+
+    /**
+     *
+     * @return the value which will be >= 0 (cast to float)
+     */
+    @Override
+    public float floatValue() {
+        return value.floatValue();
+    }
+
+    /**
+     *
+     * @return the value which will be >= 0 (cast to double)
+     */
+    @Override
+    public double doubleValue() {
+        return value.doubleValue();
+    }
+
+    @NotNull
+    public Count plus(@NotNull final BigInteger augend,
+                      @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert augend != null;
+        assert onExceedBoundary != null;
+
+        return valueOf(value.add(augend),
+                       onExceedBoundary);
+    }
+
+    @NotNull
+    public Count plus(@NotNull final BigIntegerNumber augend,
+                      @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert augend != null;
+        assert onExceedBoundary != null;
+
+        return plus(asBigInteger(augend),
+                    onExceedBoundary);
+    }
+
+    @NotNull
+    public Count plus(final long augend,
+                      @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert onExceedBoundary != null;
+
+        return plus(asBigInteger(augend),
+                    onExceedBoundary);
+    }
+
+    /**
+     *
+     * @param augend this will be truncated to an integral value (not rounded)
+     */
+    @NotNull
+    public Count plus(@NotNull final Number augend,
+                      @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert augend != null;
+        assert onExceedBoundary != null;
+
+        return plus(asBigInteger(augend),
+                    onExceedBoundary);
+    }
+
+    @NotNull
+    public Count minus(@NotNull final BigInteger subtrahend,
+                       @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert subtrahend != null;
+        assert onExceedBoundary != null;
+
+        return valueOf(value.subtract(subtrahend),
+                       onExceedBoundary);
+    }
+
+    @NotNull
+    public Count minus(@NotNull final BigIntegerNumber subtrahend,
+                       @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert subtrahend != null;
+        assert onExceedBoundary != null;
+
+        return minus(asBigInteger(subtrahend),
+                     onExceedBoundary);
+    }
+
+    @NotNull
+    public Count minus(final long augend,
+                       @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert onExceedBoundary != null;
+
+        return minus(asBigInteger(augend),
+                     onExceedBoundary);
+    }
+
+    /**
+     *
+     * @param subtrahend this will be truncated to an integral value (not rounded)
+     */
+    @NotNull
+    public Count minus(@NotNull final Number subtrahend,
+                       @NotNull final OnExceedBoundary onExceedBoundary) {
+        assert subtrahend != null;
+        assert onExceedBoundary != null;
+
+        return minus(asBigInteger(subtrahend),
+                     onExceedBoundary);
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if(this == o) {
+            return true;
+        }
+        if(o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        final Count count = (Count) o;
+
+        return value.equals(count.value);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Count{" +
+               "value=" + String.format("%,d", value) +
+               '}';
+    }
+}
+
