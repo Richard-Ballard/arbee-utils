f1c385fadd2b2e41aae3a5cf76f595131a1af324
Richard Ballard <theballards@wave.co.nz>
2016-07-07 14:24:18 +1000

    added ChangeChecker and OnChangeNotifier

---
+++ src/main/java/org/arbee/arbeeutils/misc/ChangeChecker.java
@@ -0,0 +1,71 @@
+/*
+ * (C) Copyright 2016 Richard Ballard.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.arbee.arbeeutils.misc;
+
+import net.jcip.annotations.ThreadSafe;
+import org.arbee.arbeeutils.concurrent.WrappedLock;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Optional;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
+
+/**
+ * Instances of this class have a supplier of values and hold the 'last known value' from that supplier.  When the
+ * {@link #checkForChange()} method is called it may optionally return any change from the supplier.
+ */
+@ThreadSafe
+public class ChangeChecker<T> {
+
+    @NotNull
+    private final Supplier<Optional<T>> valueSupplier;
+
+    @NotNull
+    private final WrappedLock lock;
+
+    @NotNull
+    private Optional<T> knownValue;
+
+    public ChangeChecker(@NotNull final Supplier<Optional<T>> valueSupplier) {
+        assert valueSupplier != null;
+
+        this.valueSupplier = valueSupplier;
+
+        this.lock = new WrappedLock(new ReentrantLock(false));
+        this.knownValue = Optional.empty();
+    }
+
+    @NotNull
+    public Optional<ChangedValue<T>> checkForChange() {
+        return lock.inLock(() -> {
+            Optional<ChangedValue<T>> result = Optional.empty();
+
+            final Optional<T> mostRecentValue = valueSupplier.get();
+            if(!knownValue.equals(mostRecentValue)) {
+
+                // make sure we capture 'knownValue' before resetting it
+                result = Optional.of(new ChangedValue<>(knownValue,
+                                                        mostRecentValue));
+
+                knownValue = mostRecentValue;
+            }
+
+            return result;
+        });
+    }
+
+}
